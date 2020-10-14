56d53f41d3117f47f6939d6cc00f7cb876457f1f
Ankit9454 <68605569+Ankit9454@users.noreply.github.com>
2020-10-14 12:19:59 +0530

    Update main.cpp
    added using namespace std  at starting

--- array/ex3/main.cpp
+++ array/ex3/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include "../Cube.h"
 
+using namespace std;
 using uiuc::Cube;
 
 int main() {
@@ -15,12 +16,12 @@ int main() {
   Cube cubes[3] = { Cube(11), Cube(42), Cube(400) };
   
   // Print the size of each type `Cube`:
-  std::cout << sizeof(Cube) << std::endl;
+  cout << sizeof(Cube) << endl;
 
   // Using pointer arithmetic, ask the computer to calculate
   // the offset from the beginning of the array to [2]:
   int offset = (long)&(cubes[2]) - (long)&(cubes[0]);
-  std::cout << offset << std::endl;
+  cout << offset << endl;
 
   return 0;
 }
